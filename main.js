d08f3f8b8c1261cb6724ed6a0ac809169c312c5f
Vladislav Telichko <vladislav.telichko@ya.ru>
2016-10-29 12:20:31 +0300

    Рабочий сервер.

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 'use scrict';
+var express = require('express'),
+    http = require('http'),
+    app = express();
+
+http.createServer(app).listen(8080, function(){
+    console.log('Server listening on port ' + 8080);
+});
+
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
