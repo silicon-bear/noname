88d49a840c98dc60e615f731ef89f019a486660b
Vladislav Telichko <vladislav.telichko@ya.ru>
2016-10-29 15:43:14 +0300

    Начало разработки главной страницы.

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ server.listen(conf.get('port'), function(){
     console.log('Server listening on port ' + conf.get('port'));
 });
 
+app.set('views', __dirname + '/views/');
+app.set('view engine', 'pug');
+
 app.get('/', function(req, res) {
-  res.send('hello world');
+  res.render('basic');
 });
